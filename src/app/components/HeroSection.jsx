967c1d430c9499161f28ed712f8659b62fd14751
Brandon <bforet@maisonsdumonde.com>
2024-03-26 01:46:58 +0100

    img HeroSection

--- src/app/components/HeroSection.jsx
+++ src/app/components/HeroSection.jsx
@@ -55,7 +55,7 @@ const HeroSection = () => {
         >
           <div className="rounded-full bg-[#e0d9e0] w-[250px] h-[250px] lg:w-[400px] lg:h-[400px] relative border-dark">
             <Image
-              src="/images/jul.png"
+                src="/images/herototo.png"
               alt="hero image"
               className="absolute transform -translate-x-1/2 -translate-y-1/2 top-1/2 left-1/2"
               width={300}
