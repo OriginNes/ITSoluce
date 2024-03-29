bd01902885d295c92c3c5f3085e39b39e64d13cf
Brandon <bforet@maisonsdumonde.com>
2024-03-29 18:52:01 +0100

    Add GA with vercel

--- src/app/layout.js
+++ src/app/layout.js
@@ -1,6 +1,6 @@
 import './globals.css'
-import { Inter } from 'next/font/google'
-import GoogleAnalytics from 'next-ga';
+import { Inter } from 'next/font/google';
+import { Analytics } from '@vercel/analytics/react';
 
 const inter = Inter({ subsets: ['latin'] })
 
@@ -13,7 +13,7 @@ export default function RootLayout({ children }) {
   return (
     <html lang="fr">
       <body className={inter.className}>{children}</body>
-      <GoogleAnalytics gaId="GTM-M544XBK3" />
+      <Analytics mode={'production'} />;
     </html>
   )
 }
