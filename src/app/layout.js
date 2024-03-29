dab5a81ce975eb6f126846f751615396c9916aff
Brandon <bforet@maisonsdumonde.com>
2024-03-29 18:45:12 +0100

    add GA

--- src/app/layout.js
+++ src/app/layout.js
@@ -1,5 +1,6 @@
 import './globals.css'
 import { Inter } from 'next/font/google'
+import GoogleAnalytics from 'next-ga';
 
 const inter = Inter({ subsets: ['latin'] })
 
@@ -12,6 +13,7 @@ export default function RootLayout({ children }) {
   return (
     <html lang="fr">
       <body className={inter.className}>{children}</body>
+      <GoogleAnalytics gaId="GTM-M544XBK3" />
     </html>
   )
 }
